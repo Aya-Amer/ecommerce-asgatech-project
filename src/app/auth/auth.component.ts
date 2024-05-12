e4ca30d31407928c5ef5224ed2db5b4be92c5967
Aya-Amer <ayaamer694@gmail.com>
2024-05-12 13:08:05 +0300

    sign in Auth and loader

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,10 +9,33 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  constructor(private authService: AuthService) {}
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        (error) => {
+          console.log(error);
+          this.isLoading = false;
+        }
+      );
+    }
+
     form.reset();
   }
 }
